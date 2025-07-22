fcfc7dead017652e066e5a7d8f4a465205f74963
sbx_user1051 <sbx_user1051@169.254.0.209>
2025-07-22 20:54:14 +0000

    Tue Jul 22 20:54:14 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -5,10 +5,14 @@ class CheckoutSolution:
     def checkout(self, skus):
         self._pricing = {"A": 50, "B": 30, "C": 20, "D": 15, "E": 40}
         self._offers = {
-            "A": {"quantity" : 3, "offer_price": 130},
-            "B": {"quantity" : 2, "offer_price": 45}
-        }
-
+            "A": [
+                {"quantity": 5, "offer_price": 200},
+                {"quantity": 3, "offer_price": 130}
+            ],
+            "B": [
+                {"quantity": 2, "offer_price": 45}
+            ]
+}
         self._free_items = [
             {"q_item": "E", "q_quantity": 2, "free_item": "B", "free_quantity": 1}
         ]
@@ -35,14 +39,15 @@ class CheckoutSolution:
         total = 0
         for item, count in item_counts.items():
             if item in self._offers:
-                offer = self._offers[item]
-                num_offers = count // offer["quantity"]
-                remainder = count % offer["quantity"]
-                total += num_offers * offer["offer_price"] + remainder * self._pricing[item]
-            else:
-                total += count * self._pricing[item]
+                offers = self._offers[item]
+                for offer in offers:
+                    num_offers = count // offer["quantity"]
+                    total += num_offers * offer["offer_price"]
+                    count = count % offer["quantity"]
+            total += count * self._pricing[item]
 
         return total
+
 
 
 
