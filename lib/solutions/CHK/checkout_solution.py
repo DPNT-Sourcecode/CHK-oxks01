f0d5ad57007c2decd9aa7df3b088ccb3b2194240
sbx_user1051 <sbx_user1051@169.254.0.209>
2025-07-22 20:56:03 +0000

    Tue Jul 22 20:56:03 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,9 +1,15 @@
+from collections import Counter
 
 class CheckoutSolution:
-
-    # skus = unicode string
     def checkout(self, skus):
-        self._pricing = {"A": 50, "B": 30, "C": 20, "D": 15, "E": 40}
+        self._pricing = {
+            "A": 50,
+            "B": 30,
+            "C": 20,
+            "D": 15,
+            "E": 40
+        }
+
         self._offers = {
             "A": [
                 {"quantity": 5, "offer_price": 200},
@@ -12,44 +18,45 @@ class CheckoutSolution:
             "B": [
                 {"quantity": 2, "offer_price": 45}
             ]
-}
-        self._free_items = [
+        }
+
+        self._free_item_offers = [
             {"q_item": "E", "q_quantity": 2, "free_item": "B", "free_quantity": 1}
         ]
 
-        for i in skus:
-            if i not in self._pricing:
+        for ch in skus:
+            if ch not in self._pricing:
                 return -1
-            
-        from collections import Counter
-        item_counts = Counter(skus)
 
-        for offer in self._free_items:
+        item_counts = Counter(skus)
+        free_items = Counter()
+
+        for offer in self._free_item_offers:
             q_item = offer["q_item"]
             q_quantity = offer["q_quantity"]
             free_item = offer["free_item"]
             free_quantity = offer["free_quantity"]
 
             if q_item in item_counts:
-                num_qualifiers = item_counts["q_item"] // q_quantity
-                num_free = num_qualifiers * free_quantity
-                if free_item in item_counts:
-                    item_counts[free_item] = max(0, item_counts[free_item] - num_free)
+                num_qualifiers = item_counts[q_item] // q_quantity
+                free_items[free_item] += num_qualifiers * free_quantity
 
+   
         total = 0
         for item, count in item_counts.items():
+            free_count = min(count, free_items[item]) if item in free_items else 0
+            payable_count = count - free_count
+
+  
             if item in self._offers:
                 offers = self._offers[item]
                 for offer in offers:
-                    num_offers = count // offer["quantity"]
-                    total += num_offers * offer["offer_price"]
-                    count = count % offer["quantity"]
-            total += count * self._pricing[item]
+                    num_offer = payable_count // offer["quantity"]
+                    total += num_offer * offer["offer_price"]
+                    payable_count = payable_count % offer["quantity"]
+            total += payable_count * self._pricing[item]
 
         return total
-
-
-
 
 
 
