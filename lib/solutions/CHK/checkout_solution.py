5968e1af0c0c1fbd4782f7637aae4b4fa32e559a
sbx_user1051 <sbx_user1051@169.254.0.209>
2025-07-22 20:27:13 +0000

    Tue Jul 22 20:27:13 UTC 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,8 +3,8 @@ class CheckoutSolution:
 
     # skus = unicode string
     def checkout(self, skus):
-        our_pricing = {"A": 50, "B": 30, "C": 20, "D": 15}
-        our_offers = {
+        self._pricing = {"A": 50, "B": 30, "C": 20, "D": 15}
+        self._offers = {
             "A": {"quantity" : 3, "offer_price": 130},
             "B": {"quantity" : 2, "offer_price": 45}
         }
@@ -17,15 +17,16 @@ class CheckoutSolution:
 
         total = 0
         for item, count in item_counts.items():
-            for item in our_offers:
-                offer = our_offers[item]
-                num_offers = count // our_offers["quantity"]
-                remainder = count % our_offers["quantity"]
-                total += num_offers * our_offers["price"] + remainder + our_pricing["item"]
+            if item in self._offers:
+                offer = self._offers[item]
+                num_offers = count // offer["quantity"]
+                remainder = count % offer["quantity"]
+                total += num_offers * offer["offer_price"] + remainder * self._pricing[item]
             else:
-                total += count * our_pricing["item"]
+                total += count * self._pricing[item]
 
         return total
+
 
 
 
